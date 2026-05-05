fc3a42d611aaefac56c9c8a6f3626104ae738e0d
dmmiller597 <dmmiller597@gmail.com>
2026-05-05 20:41:17 +0100

    Add data and utility imports to `__init__.py` for enhanced module accessibility. Define `__all__` for public API exposure, including key classes and functions related to contrastive learning and embedding processing.

--- src/contrasted/__init__.py
+++ src/contrasted/__init__.py
@@ -1,3 +1,30 @@
 """Contrasted: supervised contrastive learning for protein domain classification."""
 
+from contrasted.data import (
+    EmbeddingStore,
+    load_domain_ids_from_fasta,
+    parse_fasta_header,
+    read_fasta_sequences,
+)
+from contrasted.embed import EncodeConfig, ProstT5Encoder, encode_fasta
+from contrasted.model import ContrastiveModel, ProjectionHead, project
+from contrasted.search import VectorIndex
+from contrasted.utils import get_device, load_labels
+
 __version__ = "0.1.0"
+
+__all__ = [
+    "ContrastiveModel",
+    "EmbeddingStore",
+    "EncodeConfig",
+    "ProjectionHead",
+    "ProstT5Encoder",
+    "VectorIndex",
+    "encode_fasta",
+    "get_device",
+    "load_domain_ids_from_fasta",
+    "load_labels",
+    "parse_fasta_header",
+    "project",
+    "read_fasta_sequences",
+]
